d46ec1609ad12403d815397f333ef1576dd953ec
cvestone <cvestone@users.noreply.github.com>
2025-11-10 05:10:25 +0000

    Deployed - Triggered by: Update htb_ctf.md - SHA: 9dcc3dc03c3dcb7296bdc5dc3fc305a174c967d2 - Date: 2025-11-10T13:09:49+08:00 cvestone/st3n0_spAce@9dcc3dc03c3dcb7296bdc5dc3fc305a174c967d2

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/wp/251025_deadfaceCTF learning record.html","articles/wp/251107_infobahnCTF learning record.html","articles/wp/htb_ctf.html"];function toRandomPost(){
+var posts=["articles/wp/251025_deadfaceCTF learning record.html","articles/wp/251107_infobahnCTF learning record.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
