6d2fdc9b611db0d21a9066fe1836d35f6ef4e220
cvestone <cvestone@users.noreply.github.com>
2025-11-12 12:04:41 +0000

    Deployed - Triggered by: 1 - SHA: 4368465d51b0599b557c03372b18c9f845904dd9 - Date: 2025-11-12T12:03:58Z cvestone/st3n0_spAce@4368465d51b0599b557c03372b18c9f845904dd9

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["articles/wp/251025_deadfaceCTF learning record.html","articles/wp/251016_QnQSecCTF learning record.html","articles/wp/251107_infobahnCTF learning record.html"];function toRandomPost(){
+var posts=["articles/wp/251016_QnQSecCTF learning record.html","articles/wp/251025_deadfaceCTF learning record.html","articles/wp/251107_infobahnCTF learning record.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
